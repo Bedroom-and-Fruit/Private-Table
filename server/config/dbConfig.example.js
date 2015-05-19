7c09450f06b4af4f831920ad2df092dd6026e86b
Craig Smith <craigsmith24@gmail.com>
2015-05-19 16:26:00 -0700

    added server and config folders with dbConfig and secret example files

---
+++ server/config/dbConfig.example.js
@@ -0,0 +1,13 @@
+var Sequelize = require('sequelize');
+
+// Here you replace someUsername and somePassword with your prefered mysql
+// username and password.  If you would like to use something other than mysql,
+// go for it.  Check out Sequelize's docs for more info:
+// http://sequelize.readthedocs.org/en/latest/
+
+var sequelize = new Sequelize('websteamCMD', 'someUsername', 'somePassword', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
