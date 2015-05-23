0cddeb97164f5a23203b8b1012a76b575d671ae9
Henry Ng <Henry@Henrys-MacBook-Pro.local>
2015-05-22 22:24:57 -0400

    new nav bar and checkout routing

--- app/scripts/controllers/searchResultsController.js
+++ app/scripts/controllers/searchResultsController.js
@@ -2,11 +2,5 @@
 
 angular.module('privateTableApp')
   .controller('searchResultsController', ['$scope', 'SearchResults', function($scope, SearchResults) {
-    // angular.extend($scope, SearchResults);
     $scope.data = SearchResults.searchResults;
-    // as the scope.data changes, the view in ng-repeat will also change
-    // $scope.getSearchResults = function() {
-    //   $scope.data = SearchResults.searchResults;
-    // };
-
   }]);
