cf2fdf9465727f43ffae2c3e91bd1a8647d8a4c5
Craig Smith <craigsmith24@gmail.com>
2015-05-22 20:44:30 -0700

    all associations created between database table models

--- server/models/associations.js
+++ server/models/associations.js
@@ -23,32 +23,39 @@ Menu.belongsTo(Venue, {foreignKey: 'parentVenue'});
 Room.hasMany(Image, {foreignKey:'pictureOf'});
 Image.belongsTo(Room, {foreignKey: 'pictureOf'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between booking and room
+Room.hasMany(Booking, {foreignKey:'room'});
+Booking.belongsTo(Room, {foreignKey: 'room'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between room and roomAmenity
+Room.hasMany(RoomAmenity, {foreignKey:'room_ID'});
+RoomAmenity.belongsTo(Room, {foreignKey: 'room_ID'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between roomAmenity and Amenity
+Amenity.hasMany(RoomAmenity, {foreignKey:'amenities_ID'});
+RoomAmenity.belongsTo(Amenity, {foreignKey: 'amenities_ID'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between courseCombination and course
+Course.hasMany(courseCombination, {foreignKey:'course_ID'});
+CourseCombination.belongsTo(Course, {foreignKey: 'course_ID'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between coursesInMenu and menu
+Menu.hasMany(CoursesInMenu, {foreignKey:'menu_ID'});
+CoursesInMenu.belongsTo(Menu, {foreignKey: 'menu_ID'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between coursesInMenu and Course
+Course.hasMany(CoursesInMenu, {foreignKey:'course_ID'});
+CoursesInMenu.belongsTo(Course, {foreignKey: 'course_ID'});
 
-// relationship between room and image
-Room.hasMany(Image, {foreignKey:'pictureOf'});
-Image.belongsTo(Room, {foreignKey: 'pictureOf'});
+// relationship between courseCombination and menuItem
+MenuItem.hasMany(CourseCombination, {foreignKey:'menuItem_ID'});
+CourseCombination.belongsTo(MenuItem, {foreignKey: 'menuItem_ID'});
+
+
+db.sync();
+
+
+
+
 
 
