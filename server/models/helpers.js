5787ae84f1b17563eef6cbe47bdc66ba81122ac6
Craig Smith <craigsmith24@gmail.com>
2015-05-26 21:02:52 -0700

    commented out non-working code

--- server/models/helpers.js
+++ server/models/helpers.js
@@ -12,31 +12,31 @@ var RoomAmenity = require('./roomAmenities.js');
 var Amenity = require('./amenities.js');
 var Association = require('./associations.js');
 
-module.exports.getSearchResults = function(params, response) {
-  sequelize.query(
+// module.exports.getSearchResults = function(params, response) {
+//   sequelize.query(
 
-    Rooms.find({where: {}}).then(function(user) {
-      if(user) {
+//     Rooms.find({where: {}}).then(function(user) {
+//       if(user) {
 
-        if (user.checkPassword(password)) {
-          var profile = {
-            username: user.username,
-            email: user.email
-          };
-          console.log("User authenticated!");
-          response.json({token: jwt.sign(profile, secret, { expiresInMinutes: 60 * 5})});
-        } else {
-          console.log("Bad Password, Charlie");
-          response.send(401, "Wrong username or password");
-        }
-      } else {
+//         if (user.checkPassword(password)) {
+//           var profile = {
+//             username: user.username,
+//             email: user.email
+//           };
+//           console.log("User authenticated!");
+//           response.json({token: jwt.sign(profile, secret, { expiresInMinutes: 60 * 5})});
+//         } else {
+//           console.log("Bad Password, Charlie");
+//           response.send(401, "Wrong username or password");
+//         }
+//       } else {
 
-        console.log("Whoops, user doesn't exsist");
-        response.send(401, "Wrong username or password");
-      }
-    });
-    )
-};
+//         console.log("Whoops, user doesn't exsist");
+//         response.send(401, "Wrong username or password");
+//       }
+//     });
+//     )
+// };
 
 //city
 //start time
@@ -47,16 +47,16 @@ module.exports.getSearchResults = function(params, response) {
 // 'availFor'+eventType = true AND eventType+'Capacity' > guests
 
 
- sequelize.query(SELECT * FROM Rooms AS r
- WHERE city=city AND minSpend < budget AND r.id IN (
- SELECT b.room FROM bookings AS b
- WHERE (b.end < startTime )  
-     OR
- b.start > endTime);         
+//  sequelize.query(SELECT * FROM Rooms AS r
+//  WHERE city=city AND minSpend < budget AND r.id IN (
+//  SELECT b.room FROM bookings AS b
+//  WHERE (b.end < startTime )  
+//      OR
+//  b.start > endTime);         
 
 
 
-Rooms.findAll({where: r.id: req.body.params.})
+// Rooms.findAll({where: r.id: req.body.params.})
 
 
 // <= : "before"
