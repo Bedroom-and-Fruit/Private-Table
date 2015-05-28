4f71d9f6fffe55760bcc51558774a85be154541c
Henry Ng <henryfc.ng@gmail.com>
2015-05-28 15:46:32 -0400

    Update dbConfig.js

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,9 +1,9 @@
 var Sequelize = require('sequelize');
 var fs = require('fs');
 
-var username = String(fs.readFileSync(__dirname + '/databaseusername'));
-var password = String(fs.readFileSync(__dirname + '/databasepassword'));
-var databaseName = String(fs.readFileSync(__dirname + '/databasename'));
+var username = String(fs.readFileSync(__dirname + '/databaseusername')) || ''
+var password = String(fs.readFileSync(__dirname + '/databasepassword')) || ''
+var databaseName = String(fs.readFileSync(__dirname + '/databasename')) || ''
 
 if (process.env.HEROKU_POSTGRESQL_AMBER_URL) {
   var sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_AMBER_URL, {
