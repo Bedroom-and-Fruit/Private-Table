afc901cc42e11814025d9277365b3deeb6c94e2e
Lindsay Pond <lepond@gmail.com>
2015-05-30 15:06:36 -0700

    Add config in original setting

---
+++ server/config/dbConfig.js
@@ -0,0 +1,32 @@
+var Sequelize = require('sequelize');
+var fs = require('fs');
+
+if (process.env.DATABASE_URL) {
+  var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+  var sequelize = new Sequelize(match[5], match[1], match[2], {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    port: match[4],
+    host: match[3],
+    logging: console.log,
+    dialectOptions: {
+      ssl: true
+    }
+  });
+} else {
+  var username = String(fs.readFileSync(__dirname + '/databaseusername'));
+  var password = String(fs.readFileSync(__dirname + '/databasepassword'));
+  var databaseName = String(fs.readFileSync(__dirname + '/databasename'));
+  var sequelize = new Sequelize('devdev', 'tester', 'testpw', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    port: '5432',
+    host: 'ec2-23-23-81-221.compute-1.amazonaws.com',
+    logging: console.log,
+    dialectOptions: {
+      ssl: true
+    }
+  });
+}
+
+module.exports = sequelize;
