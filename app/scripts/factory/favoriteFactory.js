5ea03c3bb3ccb531eecfbb6b663bd23513e94c72
Lindsay Pond <lepond@gmail.com>
2015-06-02 08:27:44 -0700

    Create factory to manage favorites

---
+++ app/scripts/factory/favoriteFactory.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('favoriteFactory', [])
+
+.factory('Favorites', ['$location', '$http', 'SearchResults', function(SearchResults, $location, $http, SearchResults) {
+  var favorites = [];
+
+  var getFavorites = function(params) {
+    return SearchResults.getResults(params, reroute, 'api/favoriteResults', '/favorites', favorites);
+  };
+    
+  var isFavorite = function (room) {
+    if (favorites.contains(room)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var addFavorite = function (room) {
+    if (!isFavorite(room)){    
+      favorites.push(room);
+    }    
+    //make call to add favorite from database.
+  };
+
+  var removeFavorite = function (room) {
+    favorites.splice(room,1);
+    //make call to remove favorite from database.
+  };
+
+  return {
+    removeFavorite: removeFavorite,
+    addFavorite: addFavorite,
+    isFavorite: isFavorite,
+    getFavorites: getFavorites
+  };
+
+}]);
