e00d0197d77895282f6510cd7307b62996376bb1
Henry Ng <Henry@Henrys-MacBook-Pro.local>
2015-06-05 18:36:09 -0400

    changes made for checkoutbox date

--- app/scripts/controllers/searchResultsController.js
+++ app/scripts/controllers/searchResultsController.js
@@ -9,6 +9,6 @@ angular.module('privateTableApp')
     };
 
     $scope.viewRoom = function (room) {
-      roomData.viewRoom(room);
+      roomData.viewRoom(room.roomID, 'api/room/', null, roomData.reroute('/checkout/room/', room.roomID));
     };
   }]);
--- app/scripts/factory/roomFactory.js
+++ app/scripts/factory/roomFactory.js
@@ -5,25 +5,46 @@ angular.module('roomFactory', [])
 .factory('roomData', ['SearchResults', '$location', '$http', function(SearchResults, $location, $http) {
   var currentRoom;
   var roomID;
+  var bookedStartTimes = [];
+  var bookedEndTimes = [];
   var menus = [];
 
-  var viewRoom = function(room, callback) {
-    var url = 'api/room/' + room.roomID;
+  var viewRoom = function(room, url, callback, reroute) {
+    var toUrl = url + room;
     return $http({
       method: 'GET',
-      url: url
+      url: toUrl
     })
-    .then(function(response){
-      // console.log(response.data);
+    .then(function(response) {
+      if (response.data.venueName) {
       roomID = response.data.id;
       currentRoom = response.data;
       currentRoom.menuPrices = currentRoom.menuPrices.sort(function(a,b){return a-b;});
+      }
+
+      if (response.data.bookedStartTimes) {
+        bookedStartTimes.splice(0, bookedStartTimes.length);
+        response.data.bookedStartTimes.forEach(function(val){
+          bookedStartTimes.push(val);
+        });
+        bookedEndTimes.splice(0, bookedEndTimes.length);
+        response.data.bookedEndTimes.forEach(function(val){
+          bookedEndTimes.push(val);
+        });
+      }
+      
       if (callback) {
         callback();
       }
-      $location.url($location.path());
-      $location.path('/checkout/room/'+roomID);
+      if (reroute) {
+        reroute();
+      }
       });
+  };
+
+  var reroute = function(url, roomID) {
+    $location.url($location.path());
+    $location.path(url + roomID);
   };
 
   var getRoom = function() {
@@ -31,11 +52,24 @@ angular.module('roomFactory', [])
     return currentRoom;
   };
 
+  var getStartTimes = function() {
+    return bookedStartTimes;
+  };
+
+  var getEndTimes = function() {
+    return bookedEndTimes;
+  };
+
   return {
     menus: menus,
     currentRoom: currentRoom,
     viewRoom: viewRoom,
-    getRoom: getRoom
+    getRoom: getRoom,
+    bookedStartTimes: bookedStartTimes,
+    bookedEndTimes: bookedEndTimes,
+    reroute: reroute,
+    getStartTimes: getStartTimes,
+    getEndTimes: getEndTimes
   };
 
 }]);
