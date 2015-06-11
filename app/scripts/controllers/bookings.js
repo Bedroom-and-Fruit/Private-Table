a61aaa47b7225f9a1ba17e27fe6c48a14e383a6e
Lindsay Pond <lepond@gmail.com>
2015-06-10 21:34:51 -0700

    Refine Bookings view.

--- app/scripts/controllers/bookings.js
+++ app/scripts/controllers/bookings.js
@@ -3,11 +3,6 @@ angular.module('privateTableApp')
   .controller('bookingsController', ['$scope', 'Bookings', function($scope, Bookings) {
     //This controller determines how the ngRepeat in searchResults view is filtered.
     $scope.Bookings = Bookings;
-    $scope.detailsReveal = false;
-
-    $scope.showDetails = function(booking) {
-      $scope.Bookings.currentBooking = booking;
-      $scope.detailsReveal = true;
   
     $scope.init = function () {
       Bookings.getAllBookings();
