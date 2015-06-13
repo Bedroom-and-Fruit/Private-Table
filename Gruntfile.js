ad943f0a1eecc67514a9f21b921fe916b2fa4eb7
Craig Smith <craigsmith24@gmail.com>
2015-06-13 16:26:27 -0700

    more updates to accommodate addition of Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,9 @@ module.exports = function (grunt) {
     },
 
     uglify: {
+      options: {
+        mangle: false
+      },
       build: {
         src: 'app/build/production.js',
         dest: 'app/build/production.min.js'
